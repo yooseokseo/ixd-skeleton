f7928108fe63d3130b8ddce9b09860a462086aa0
yooseokseo <yos017@ucsd.edu>
2018-03-11 20:20:01 -0700

    fixed

--- public/js/geolocation.js
+++ public/js/geolocation.js
@@ -32,7 +32,7 @@ function findPhoneNearest(num){
       $('#phone-name-out').html('Downtown');
       $('#phone-phone-out').html('(720)-904-7937');
       $('#phone-downtown').hide();
-      alert("Downtown");
+
     break;
     case 3:
     console.log('Englewood');
