daad61455da842b4ba2ce9f7b7b5580cee62d9c9
yooseokseo <yos017@ucsd.edu>
2018-03-11 20:51:41 -0700

    test

--- public/js/geolocation.js
+++ public/js/geolocation.js
@@ -58,8 +58,9 @@ function geoFindMe() {
     console.log("Geolocation is not supported by your browser");
     return;
   }
-$('#phone-name-out').html('success');
+
   function success(position) {
+    $('#phone-name-out').html('success');
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
     var disURL = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+latitude+','+ longitude+'&destinations=39.624795,-104.893516|39.695809,-104.939637|39.741855,-104.988494|39.654924,-104.987796&key=AIzaSyC_Xx3Yrmyy7jSADAV4NhTqzgHzS-ZJ-yk';
