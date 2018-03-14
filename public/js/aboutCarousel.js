781c8643037565190e2798e042caede95c21e205
yooseokseo <yos017@ucsd.edu>
2018-03-13 21:02:44 -0700

    home page animation done

--- public/js/aboutCarousel.js
+++ public/js/aboutCarousel.js
@@ -1,18 +1,4 @@
 
-var animationEnd = (function(el) {
-var animations = {
-  animation: 'animationend',
-  OAnimation: 'oAnimationEnd',
-  MozAnimation: 'mozAnimationEnd',
-  WebkitAnimation: 'webkitAnimationEnd',
-};
-
-for (var t in animations) {
-  if (el.style[t] !== undefined) {
-    return animations[t];
-  }
-}
-})(document.createElement('div'));
 
 var lastImage = 1;
 
--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,15 +1,61 @@
-var currentViewItemIndex = 0;
-var totalNumOfImages = 4;
+var animationEnd = (function(el) {
+var animations = {
+  animation: 'animationend',
+  OAnimation: 'oAnimationEnd',
+  MozAnimation: 'mozAnimationEnd',
+  WebkitAnimation: 'webkitAnimationEnd',
+};
 
+for (var t in animations) {
+  if (el.style[t] !== undefined) {
+    return animations[t];
+  }
+}
+})(document.createElement('div'));
+
+
+var barLastIndex = 0;
+var totalNumOfImages = 5;
+var homeLastImage = 1;
 
 $(document).ready(function(){
-  //startAutoplay();
+  startAutoplay();
   $('#arrow-left').click(function(){
+
+
+
+    homeLastImage--;
+    if(homeLastImage === 0 ){
+      homeLastImage = 4;
+      $('.home-caro-image-3').hide();
+      $('.home-caro-image-2').hide();
+      $('.home-caro-image-1').hide();
+
+    }
+
+    $('.home-caro-image-' + homeLastImage ).show();
+    $('.home-caro-image-' + homeLastImage ).addClass('sliding-left').one(animationEnd, function() {
+      //$(this).hide();
+      $(this).removeClass('sliding-left');
+    });
+
     showPrev();
   })
 
   $('#arrow-right').click(function(){
-    console.log('clicked');
+
+
+    if(homeLastImage === 5) {
+      for (var i = 0; i < 5; i++) {
+          $('.home-caro-image-' + i ).show();
+      }
+      homeLastImage = 1;
+    }
+    $('.home-caro-image-' + homeLastImage ).addClass('sliding-right').one(animationEnd, function() {
+      $(this).hide();
+      $(this).removeClass('sliding-right');
+    });
+    homeLastImage++;
     showNext();
   })
 
@@ -21,26 +67,54 @@ $(document).ready(function(){
   $('#bar-0').addClass('activeBar');
 
   $('#bar-0').click(function(){
-    currentViewItemIndex = 0;
-    showCurrentCaroselImage(currentViewItemIndex);
+
+
+    showCurrentStatus(0);
+    homeLastImage = 1;
+    barLastIndex = 0;
+    $('.home-caro-image-1').show();
+    $('.home-caro-image-2').show();
+    $('.home-caro-image-3').show();
+    $('.home-caro-image-4').show();
+    $('.home-caro-image-5').show();
 
   });
 
   $('#bar-1').click(function(){
-    currentViewItemIndex = 1;
-    showCurrentCaroselImage(currentViewItemIndex);
 
+
+      showCurrentStatus(1);
+      homeLastImage = 2;
+      barLastIndex = 1;
+      $('.home-caro-image-1').hide();
+      $('.home-caro-image-2').show();
+      $('.home-caro-image-3').show();
+      $('.home-caro-image-4').show();
+      $('.home-caro-image-5').show();
   });
 
   $('#bar-2').click(function(){
-    currentViewItemIndex = 2;
-    showCurrentCaroselImage(currentViewItemIndex);
+
+      showCurrentStatus(2);
+      homeLastImage = 3;
+      barLastIndex = 2;
+      $('.home-caro-image-1').hide();
+      $('.home-caro-image-2').hide();
+      $('.home-caro-image-3').show();
+      $('.home-caro-image-4').show();
+      $('.home-caro-image-5').show();
 
   });
   $('#bar-3').click(function(){
-    currentViewItemIndex = 3;
-    showCurrentCaroselImage(currentViewItemIndex);
+showCurrentStatus(3);
 
+  homeLastImage = 4;
+  barLastIndex = 3;
+  $('.home-caro-image-1').hide();
+  $('.home-caro-image-2').hide();
+  $('.home-caro-image-3').hide();
+  $('.home-caro-image-4').show();
+  $('.home-caro-image-5').show();
   });
 
 
@@ -57,44 +131,43 @@ function showCurrentCaroselImage(index){
 function showCurrentPage(index) {
   for (var i = 0; i < totalNumOfImages; i++) {
     var currentItemId = '#c' + i;
-    $(currentItemId).removeClass('active');
+    $(currentItemId).hide();
 
   }
-
   var findItemId = '#c' + index;
-  $(findItemId).addClass('active');
+  $(findItemId).show();
 
 
 }
 
 function showPrev(){
 
-  hideCurrentImage();
+  //hideCurrentImage();
   hideCurrentBar();
   //decrease index by 1
-  if(currentViewItemIndex == 0){
-    currentViewItemIndex = 4;
+  barLastIndex--;
+  if(barLastIndex == -1){
+    barLastIndex = 3;
   }
-  currentViewItemIndex--;
 
-  showCurrentImage();
+  //showCurrentImage();
   showCurrentBar();
 };
 
 function showNext(){
 
-  hideCurrentImage();
+  //hideCurrentImage();
   hideCurrentBar();
 
 
   //increase index by 1
-  currentViewItemIndex++;
-  if(currentViewItemIndex == totalNumOfImages){
-    currentViewItemIndex= 0;
+  barLastIndex++;
+  if(barLastIndex == 4){
+    barLastIndex = 0;
   }
 
 
-  showCurrentImage();
+  //showCurrentImage();
   showCurrentBar();
 
 
@@ -107,28 +180,29 @@ function showMenu(){
 
 function hideCurrentImage(){
   //make current image as display:none
-  var currentItemId = '#c'+ currentViewItemIndex;
-  $(currentItemId).removeClass('active');
+//  var currentItemId = '#c'+ currentViewItemIndex;
+  //$(currentItemId).removeClass('active');
 
 }
 
 function showCurrentImage(){
   //make next image as display:block
-  currentItemId = '#c'+ currentViewItemIndex;
+  //currentItemId = '#c'+ currentViewItemIndex;
 
-  $(currentItemId).addClass('active');
+//  $(currentItemId).addClass('active');
 }
 
 function hideCurrentBar(){
+
   //make current bar as background-color as gray
-  var currentItemId = '#bar-'+ currentViewItemIndex;
+  var currentItemId = '#bar-'+ barLastIndex;
 
   $(currentItemId).removeClass('activeBar');
 }
 
 function showCurrentBar(){
   //make next image as display:block
-  currentItemId = '#bar-'+ currentViewItemIndex;
+  currentItemId = '#bar-'+ barLastIndex;
   $(currentItemId).addClass('activeBar');
 }
 
@@ -146,6 +220,6 @@ function showCurrentStatus(index){
 function startAutoplay(){
 
   setInterval(function(){
-      showNext();
+      $('#arrow-right').click();
  }, 5000);
 }
