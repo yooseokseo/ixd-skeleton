c653f413bed4521fdc0a2fbc9d90e8bf42173a33
yooseokseo <yos017@ucsd.edu>
2018-03-14 16:24:21 -0700

    sliding animation

--- public/js/about.js
+++ public/js/about.js
@@ -4,8 +4,21 @@ var lastImageAboutDesk = 1;
 
 
 $(document).ready(function(){
-  //startAutoplayAbout();
+  startAutoplayAbout();
   $('#arrow-left-about').click(function(){
+    lastImageAboutDesk--;
+    if(lastImageAboutDesk === 0){
+      lastImageAboutDesk = 4;
+      $('#c3-about').hide();
+      $('#c2-about').hide();
+      $('#c1-about').hide();
+    }
+    $('#c'+lastImageAboutDesk+'-about').show();
+    $('#c'+lastImageAboutDesk+'-about').addClass('sliding-right').one(animationEnd, function() {
+      //$(this).hide();
+      $(this).removeClass('sliding-right');
+
+    });
     showPrevAbout();
 
   })
@@ -34,29 +47,64 @@ $(document).ready(function(){
   $('#bar-0-about').addClass('activeBar');
 
   $('#bar-0-about').click(function(){
-    currentViewItemIndexAbout = 0;
+    showCurrentStatusAbout(0);
     //showCurrentCaroselImageAbout(currentViewItemIndexAbout);
-
+    lastImageAboutDesk = 1;
+    currentViewItemIndexAbout = 0;
+    $('#c1-about').show();
+    $('#c2-about').show();
+    $('#c3-about').show();
+    $('#c4-about').show();
+    $('#c5-about').show();
   });
 
   $('#bar-1-about').click(function(){
+    showCurrentStatusAbout(1);
+    lastImageAboutDesk = 2;
     currentViewItemIndexAbout = 1;
-    //showCurrentCaroselImageAbout(currentViewItemIndexAbout);
+    $('#c1-about').hide();
+    $('#c2-about').show();
+    $('#c3-about').show();
+    $('#c4-about').show();
+    $('#c5-about').show();
 
   });
 
   $('#bar-2-about').click(function(){
+    showCurrentStatusAbout(2);
+    lastImageAboutDesk = 3;
     currentViewItemIndexAbout = 2;
-    //showCurrentCaroselImageAbout(currentViewItemIndexAbout);
+    $('#c1-about').hide();
+    $('#c2-about').hide();
+    $('#c3-about').show();
+    $('#c4-about').show();
+    $('#c5-about').show();
+
 
   });
   $('#bar-3-about').click(function(){
-    currentViewItemIndexAbout = 3;
-    //showCurrentCaroselImageAbout(currentViewItemIndexAbout);
-
+    showCurrentStatusAbout(3);
+    lastImageAboutDesk = 4;
+    currentViewItemIndexAbout = 33;
+    $('#c1-about').hide();
+    $('#c2-about').hide();
+    $('#c3-about').hide();
+    $('#c4-about').show();
+    $('#c5-about').show();
   });
 
 });
+function showCurrentStatusAbout(index){
+  //make next bars as background-color as gray
+  for (var i = 0; i < totalNumOfImages ; i++) {
+    var currentBarId = '#bar-'+ i+'-about';
+      $(currentBarId).removeClass('activeBar');
+  }
+  currentBarId = '#bar-'+ index+'-about';
+  $(currentBarId).addClass('activeBar');
+
+}
+
 
 function showCurrentCaroselImageAbout(index){
   showCurrentPageAbout(index);
@@ -82,10 +130,11 @@ function showPrevAbout(){
   //hideCurrentImageAbout();
   hideCurrentBarAbout();
   //decrease index by 1
-  if(currentViewItemIndexAbout == 0){
-    currentViewItemIndexAbout = 4;
-  }
   currentViewItemIndexAbout--;
+  if(currentViewItemIndexAbout == -1){
+    currentViewItemIndexAbout = 3;
+  }
+
 
   //showCurrentImageAbout();
   showCurrentBarAbout();
@@ -99,7 +148,7 @@ function showNextAbout(){
 
   //increase index by 1
   currentViewItemIndexAbout++;
-  if(currentViewItemIndexAbout == totalNumOfImages){
+  if(currentViewItemIndexAbout == 4){
     currentViewItemIndexAbout= 0;
   }
 
