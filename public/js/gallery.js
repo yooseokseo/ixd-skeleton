7d3e78ea97096b9260ef784ffb7b1ed74b50d99f
yooseokseo <yos017@ucsd.edu>
2018-03-19 22:05:02 -0700

    gallery done

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -18,3 +18,30 @@ function hideAllShareIcons(){
 }
 
 */
+
+$('.g-food-btn').click(function(){
+  $('.g-interior-btn').removeClass('gallery-active');
+  $('.g-community-btn').removeClass('gallery-active');
+  $(this).addClass('gallery-active');
+  $('.food-p').show();
+  $('.interior-p').hide();
+  $('.community-p').hide();
+});
+
+$('.g-interior-btn').click(function(){
+  $('.g-food-btn').removeClass('gallery-active');
+  $('.g-community-btn').removeClass('gallery-active');
+  $(this).addClass('gallery-active');
+  $('.food-p').hide();
+  $('.interior-p').show();
+  $('.community-p').hide();
+});
+
+$('.g-community-btn').click(function(){
+  $('.g-interior-btn').removeClass('gallery-active');
+  $('.g-food-btn').removeClass('gallery-active');
+  $(this).addClass('gallery-active');
+  $('.food-p').hide();
+  $('.interior-p').hide();
+  $('.community-p').show();
+});
